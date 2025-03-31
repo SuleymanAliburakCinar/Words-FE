e53d22fdf6961aa8c0e7a255a68e6e751dee2def
Suley <sabc4129@gmail.com>
2025-03-31 21:11:25 +0300

    Summary Card Added

---
+++ src/component/SummaryCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./SummaryCard.css";
+
+const SummaryCard = ({correct, total, successRate, difficulty}) => {
+
+    return (
+      <div className="card-container">
+        <div className="summary-card">
+            <h2 className="summary-title">📜 Quiz Summary</h2>
+            <p className="summary-info">❓ Total Question: <strong>{total}</strong></p>
+            <p className="summary-info">✅ Correct Answer: <strong>{correct}</strong></p>
+            <p className="summary-info">📊 Success Rate: <strong>%{successRate}</strong></p>
+            <p className="summary-info">🔥 Difficulty: <strong>{difficulty}</strong></p>
+        </div>
+      </div>
+    );
+};
+
+export default React.memo(SummaryCard);
